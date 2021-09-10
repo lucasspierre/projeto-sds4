9fc57746c5d028f2159750fbfa5f6c0a1bd645c1
lucasspierre <lucaspierrealmeida@gmail.com>
2021-09-10 14:16:34 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,19 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" exact>
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
